ce46697b677a0ed3c5ee5921dc847456d91261ef
Juan Carlos <juancarlos.gubbins@gmail.com>
2020-03-17 17:23:57 -0300

    test

---
+++ Laboratorio 01 jcgubbins99/Laboratorio 01 jcgubbins99/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laboratorio_01_jcgubbins99
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
