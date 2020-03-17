c8e89ce7fb4cb56ed642646a510d7f9da36e712e
Juan Carlos <juancarlos.gubbins@gmail.com>
2020-03-17 17:40:07 -0300

    Lab1

--- Laboratorio 01 jcgubbins99/Laboratorio 01 jcgubbins99/Program.cs
+++ Laboratorio 01 jcgubbins99/Laboratorio 01 jcgubbins99/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 
 namespace Laboratorio_01_jcgubbins99
-{
-    class Program
+{  //Alumno: Juan Carlos Gubbins  
+    public class Persona    //Se crea la clase persona
     {
-        static void Main(string[] args)
+        //Atributos
+        public string Nombre;
+
+        public string Apellido;
+
+        public Persona(string _nombre, string _apellido) //constructor
         {
-            Console.WriteLine("Hello World!");
+            Nombre = _nombre;
+            Apellido = _apellido;
+        }
+        //Método o acción
+        public int Lanzar()
+        {
+
+            Random random = new Random();
+            return random.Next(0, 3); //piedra(0), papel(1) o tijeras(2) numero aleatorio
+        }
+
+    }
+
+    class Program    //Clase la cual va a llamar a la clase Persona a realizar su acción "Lanzar"
+    {
+        static void Main()
+        {
+            Persona p1 = new Persona("Bob", "Kunga");
+            int Lanza1 = 0; //Variable =0
+            Lanza1 = p1.Lanzar();
+
+            Console.WriteLine(Lanza1);
+
+
+
         }
     }
 }
